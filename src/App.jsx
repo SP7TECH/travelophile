1fe70527fe73c31c4bc2a37b66dd300ca113cf9b
SP7TECH <spandey247789@gmail.com>
2025-03-27 23:40:47 +0530

    dynamic routes, URL Params

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Login from "./pages/Login";
 import { useEffect, useState } from "react";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
 
 const BASE_URL = `http://localhost:3002`;
 
@@ -49,6 +50,7 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
