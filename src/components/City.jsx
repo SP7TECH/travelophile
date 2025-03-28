3fc614bacce7dbd4cdb216c02424771850eac0c5
SP7TECH <spandey247789@gmail.com>
2025-03-28 10:26:39 +0530

    query string and search params hook

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -21,8 +21,19 @@ function City() {
   const { cityName, emoji, date, notes } = currentCity;
 
   const { id } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
 
-  return <h1>City {id}</h1>;
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <>
+      <h1>City {id}</h1>
+      <h2>
+        Position: {lat}, {lng}
+      </h2>
+    </>
+  );
 
   // return (
   //   <div className={styles.city}>
